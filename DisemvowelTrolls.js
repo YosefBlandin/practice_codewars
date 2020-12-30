837748c16b1d28615e977269cee4be774c05cf98
Yosef Blandin <yosefleanb@gmail.com>
2020-12-29 22:24:29 -0400

    Exercise: DisemvowelTrolls

---
+++ DisemvowelTrolls.js
@@ -0,0 +1,8 @@
+function disemvowel(str) {
+    const vowels = /[AEIOUaeiou]/g;
+    str = str.replace(vowels, '')
+    
+    return console.log(ave);
+  }
+
+  disemvowel("This website is for losers LOL!")
